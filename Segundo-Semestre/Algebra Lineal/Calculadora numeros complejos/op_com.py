2dfda7c9d2c072377eaaa2abfb2e88a115883f48
Dennis Ariel Ake Lopez <dariel_504@hotmail.com>
2020-08-28 03:42:42 -0500

    revisión de ejercicio y coreccion de errores

--- Segundo-Semestre/Algebra Lineal/Calculadora numeros complejos/op_com.py
+++ Segundo-Semestre/Algebra Lineal/Calculadora numeros complejos/op_com.py
@@ -1,39 +1,42 @@
 
 import cmath
 def suma():
-    a = (input("Ingrese primer numero\n"))
-    b = (input("Ingrese otro numero\n"))
+    a = (input("Ingrese la primera cantidad usando este formato ->10+25j\n"))
+    b = (input("Ingrese la segunda cantidad\n"))
     a2= complex(a)
     b2= complex(b)
     resultado= a2+b2
     print("El resultado de la suma es:",resultado)
+    print(" ")
 def resta():
-    a=input("Introduce el primer numero\n")
-    b=input("Introduce el primer numero\n")
+    a=(input("Ingrese la primera cantidad usando este formato ->10+25j\n"))
+    b=(input("Ingrese la segunda cantidad\n"))
     a2= complex(a)
     b2= complex(b)
     resultado= a2-b2
     print("El resultado de la resta es:",resultado)
+    print(" ")
 def division():
-    a=input("Introduce el primer numero\n")
-    b= input("ntroduce el primer numero\n")
+    a=input(("Ingrese la primera cantidad usando este formato ->10+25j\n"))
+    b=input(("Ingrese la segunda cantidad\n"))
     a2= complex(a)
     b2= complex(b)
     resultado= a2/b2
     print("El resultado de la division es:",resultado)
+    print(" ")
 def multiplicacion():
-    a=input("Introduce el primer numero\n")
-    b=input("Introduce el primer numero\n")
+    a=input("Ingrese la primera cantidad usando este formato ->10+25jn")
+    b=input("Ingrese la segunda cantidad\n")
     a2= complex(a)
     b2= cmplex(b)
     resultado= a2*b2
     print("El resultado de la multiplicacion es:",resultado)
-
+    print(" ")
 while (True):
-        print ("1 = suma")
-        print ("2 = resta")
-        print ("3 = multiplicacion")
-        print ("4 = division")
+        print ("1 = Suma")
+        print ("2 = Resta")
+        print ("3 = Multiplicacion")
+        print ("4 = Division")
         opcion = int(input("Ingrese opcion\n"))
         if (opcion == 1):
             suma()
